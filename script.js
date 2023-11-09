ea7b36cec9190a647d995a05094e0c2e2dbc68a6
sinhaadyant <92117021+sinhaadyant@users.noreply.github.com>
2023-11-10 00:53:42 +0530

    remove unused code

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-anime
-  .timeline({ loop: true })
-  .add({
-    targets: ".ml15 .word",
-    scale: [14, 1],
-    opacity: [0, 1],
-    easing: "easeOutCirc",
-    duration: 800,
-    delay: (el, i) => 800 * i,
-  })
-  .add({
-    targets: ".ml15",
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000,
-  });
-
 const input = document.getElementById("name-input");
 const formButton = document.getElementById("form-button");
 
@@ -41,5 +23,3 @@ formButton.addEventListener("click", () => {
     window.location.href = newUrl;
   }
 });
-
-/////
